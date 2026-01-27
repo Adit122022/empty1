17c14542180691d591de4c17b1b231a3ccf6345e
Adit122022 <adity202212@gmail.com>
2026-01-27 21:57:54 +0530

    Starting of a new era

--- TODO/FRONTEND/Backend/src/Models/Todo.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const TodoSchema = new mongoose.Schema({
-    task:String,
-    done:{type:Boolean , default:false}
-});
-
-module.exports = mongoose.model('todos', TodoSchema);
